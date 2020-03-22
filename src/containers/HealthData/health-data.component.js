8c3c6d737531497ca0b136f4f7c33cc53ce9c42e
Pedro Pinto <pedropinto@Pedros-MacBook-Pro-2.local>
2020-03-22 21:38:32 +0000

    Code clean up on the health-data component

--- src/containers/HealthData/health-data.component.js
+++ src/containers/HealthData/health-data.component.js
@@ -2,78 +2,19 @@
 /* eslint-disable no-nested-ternary */
 /* eslint-disable no-console */
 
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useTranslation } from "react-i18next";
-import SolidAuth from "solid-auth-client";
-import { successToaster, errorToaster } from "@utils";
-import { Table, Tr } from "styled-table-component";
-import {
-  describeSubject,
-  VirtualDocument,
-  describeDocument,
-  describeContainer,
-  Reference,
-  fetchDocument
-} from "plandoc";
-import { space, solid, rdf, schema } from "rdf-namespaces";
+
+import { rdf, schema } from "rdf-namespaces";
 import NotesComponent from "./notes-component";
-import { addPermission, createFileACLIfNotExists } from "./acl-utils";
 import {
   TextEditorWrapper,
   TextEditorContainer,
   Header,
   Form,
   FullGridSize,
-  Button,
-  Label,
-  Input,
   WebId
 } from "./health-data.style";
-import { SocialEntity } from "rdf-namespaces/dist/contact";
-
-const getPodData = async webId => {
-  const profile = describeSubject().isFoundAt(webId);
-  const storage = describeContainer().isFoundOn(profile, space.storage);
-  const publicTypeIndex = describeDocument().isFoundOn(
-    profile,
-    solid.publicTypeIndex
-  );
-
-  const notesTypeRegistration = describeSubject()
-    .isEnsuredIn(publicTypeIndex)
-    .withRef(rdf.type, solid.TypeRegistration)
-    .withRef(solid.forClass, schema.TextDigitalDocument);
-
-  const notesDoc = describeDocument().isEnsuredOn(
-    notesTypeRegistration,
-    solid.instance,
-    storage
-  );
-
-  //Create the document if one does not exist already
-  const notesFetchedDocument = await fetchDocument(notesDoc);
-  let fileUrl = notesFetchedDocument.getAcl().replace(".acl", "");
-
-  //create ACL if one does not exist already
-  createFileACLIfNotExists(fileUrl, webId);
-
-  //Add Note
-  //await addNote("New Note " + new Date(), notesFetchedDocument);
-
-  // List Notes
-  let notes = notesFetchedDocument
-    .getSubjectsOfType(schema.TextDigitalDocument)
-    .map(x => x.getString(schema.text));
-  console.log("Note Debug:", notes);
-
-  //Share with other users
-  addPermission(fileUrl, webId, [
-    "https://pedropiloto.solid.community/profile/card#me",
-    "https://pedropiloto2.solid.community/profile/card#me"
-  ]);
-
-  return notes;
-};
 
 async function addNote(note, notesList) {
   const newNote = notesList.addSubject();
